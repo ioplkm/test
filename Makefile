cd183c2642836ef663458ba35963e71e5ffccaa2
ioplkm <ioplkm352@gmail.com>
2022-08-15 00:17:51 +0500

    wayland

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = clang
-CFLAGS = -Wall -Wextra -Wpedantic -pipe -O3 -s -std=c99
+CFLAGS = -Wall -Wextra -Wpedantic -pipe -O3 -s -std=c99 -D_POSIX_C_SOURCE=200112L
 LDFLAGS = -L/usr/local/lib64
-LDLIBS = -lge -lpe
+LDLIBS = -lge -lpe -lwayland-client -lpthread
 all: main.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $(LDLIBS) main.c -o main
 clean:
--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 
 #include <unistd.h>
-
-//#include "inc/pointspring.h"
-//#include "inc/pointcable.h"
+#include <pthread.h>
 
 #include <pe/vector.h>
 #include <pe/matrix.h>
@@ -19,6 +17,7 @@
 #include <ge/fb.h>
 #include <ge/cam.h>
 #include <ge/draw.h>
+#include <ge/wl.h>
 
 #define dTime 1/640.0
 #define G 9.81
@@ -39,12 +38,31 @@ void printCollision(Collision *pC) {
 Matrix33 cubeiit = {6.0/16.0, 0, 0, 0, 6.0/16.0, 0, 0, 0, 6.0/16.0};
 Matrix34 null34 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+Camera cam;
+CollisionBox cb;
+CollisionBox cb2;
+
+void *wl() {
+  uint32_t *b = init();
+  while (wl_display_dispatch(wl_display)) {
+    for (int i = 0; i < 1080; i++)
+      for (int j = 0; j < 1920; j++)
+        b[i*1920+j] = 0;
+    drawBox(b, &cam, &cb, red);
+    drawBox(b, &cam, &cb2, blue);
+  }
+  end();
+  return NULL;
+}
+
 int main() {
+  pthread_t tid;
+  pthread_create(&tid, NULL, wl, NULL);
 
   Collision collisions[999];
   int collisionC = 0;
 
-  uint32_t *b = fbInit();
+  //uint32_t *b = fbInit();
   //Rigidbody rb = {{2, 10, 0}, {0, 0, 0}, {0, 0, 0}, {0, -G, 0}, {0, 0, 0}, {0, 0, 0}, {1, 0.01, 0.01, 0.01}, 1, cubeiit, null34};
   //Rigidbody rb = {{3, 10, 0}, {0, 0, 0}, {0, 0, 0}, {0, -G, 0}, {0, 0, 0}, {0, 0, 0}, {0.92, 0, 0, 0.38}, 1, cubeiit, null34};
   Rigidbody rb = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {1, 0, 0, 0}, 1, cubeiit, null34};
@@ -54,15 +72,15 @@ int main() {
   rb2.o = qNorm(rb2.o);
   rb.transform = m34FromQV(rb.o, rb.p);
   rb2.transform = m34FromQV(rb2.o, rb2.p);
-  CollisionBox cb = {&rb, {0, 0, 0}, {2, 2, 2}};
-  CollisionBox cb2 = {&rb2, {0, 0, 0}, {2, 2, 2}};
+  cb = (CollisionBox){&rb, {0, 0, 0}, {2, 2, 2}};
+  cb2 = (CollisionBox){&rb2, {0, 0, 0}, {2, 2, 2}};
 
-  Camera cam = {{0, 0, -10}, 100, {1, 0, 0, 0}, null34};
+  cam = (Camera){{0, 0, -10}, 100, {1, 0, 0, 0}, null34};
   cam.transform = m34FromQV(cam.o, cam.p);
 
   ConvexPolyhedra ph = {&rb, {0, 0, 0}, {2, 2, 2}};
   ConvexPolyhedra ph2 = {&rb2, {0, 0, 0}, {2, 2, 2}};
-  
+
   for (;;) {
     updateRigidbody(&rb, dTime);
     updateRigidbody(&rb2, dTime);
@@ -79,16 +97,7 @@ int main() {
       printCollision(&collisions[i]);
     }
 
-    /*drawPolyhedra(b, &cam, &ph, red);
-    drawPolyhedra(b, &cam, &ph2, blue);
-    usleep((int)(1000000*dTime));
-    drawPolyhedra(b, &cam, &ph, black);
-    drawPolyhedra(b, &cam, &ph2, black);*/
-    drawBox(b, &cam, &cb, red);
-    drawBox(b, &cam, &cb2, blue);
-    usleep((int)(1000000*dTime));
-    drawBox(b, &cam, &cb, black);
-    drawBox(b, &cam, &cb2, black);
+    usleep((int)(1000000*dTime*10));
   }
 }
 
